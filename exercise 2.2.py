abc5ad13ec25ced13d3f61b01695563ffece2343
I Mutiara <issacianmutiara@gmail.com>
2021-03-10 12:38:42 +0700

    Tugas 2.2

---
+++ exercise 2.2.py
@@ -0,0 +1,31 @@
+#proses memasukkan data ke variabel
+nama = "Issacian Mutiara Paska"
+#proses mencetak variabel
+print(nama)
+
+#nilai dan tipe data dalam variabel dapat diubah
+umur = 19               #nilai awal
+print(umur)             #mencetak nilai umur
+type(umur)              #mengecek tipe data umur
+umur = "sembilan belas" #nilai setelah diubah
+print(umur)             #mencetak nilai umur
+type(umur)              #mengecek tipe data umur
+
+namaDepan = "Issacian"
+namaTengah = "Mutiara"
+namaBelakang = "Paska"
+nama = namaDepan + " " + namaTengah + " " + namaBelakang
+umur = 19
+hobi = "Naik Sepeda"
+print("Biodata\n", nama, "\n", umur, "\n", hobi)
+
+#contoh variabel
+inivariabel = "Hello"
+ini_jugavariabel = "Hai"
+_inivariabeljuga = "Halo"
+inivariabel333 = "bye juga"
+
+panjang = 12
+lebar = 7
+luas = panjang*lebar
+print(luas)

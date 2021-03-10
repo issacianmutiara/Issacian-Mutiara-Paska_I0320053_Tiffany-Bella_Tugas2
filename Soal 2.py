33b7667ca3642815f9e726c811af54c8498a63d7
I Mutiara <issacianmutiara@gmail.com>
2021-03-10 23:12:35 +0700

    Soal 1

---
+++ Soal 2.py
@@ -0,0 +1,53 @@
+#tipe data string
+tipe_data = "data string"
+print("tipe :",tipe_data)
+nama_lengkap = "Issacian Mutiara Paska"
+print("nama lengkap :",nama_lengkap)
+nama_panggilan = "Issa"
+print("nama panggilan :",nama_panggilan)
+jenis_kelamin = "perempuan"
+print("jenis kelamin :",jenis_kelamin)
+alamat = "Perumahan Puri Malangjiwan Indah,Colomadu,Karanganyar"
+print("alamat :",alamat)
+agama = "katolik"
+print("agama :",agama)
+hobi = "bersepeda,bersih-bersih"
+print("hobi :",hobi)
+status = "belum kawin"
+print("status :",status)
+golongan_darah = "A"
+print("golongan darah :",golongan_darah)
+kewarganegaraan = "Indonesia"
+print("kewarganegaraan :",kewarganegaraan)
+print(" ")
+#tipe data integer
+tipe_data = "data integer"
+print("tipe :",tipe_data)
+umur_tahun = 18
+umur_bulan = 11
+umur_hari = 20
+print("umur saya adalah :",umur_tahun, "tahun", umur_bulan, "bulan", umur_hari, "hari")
+berat_badan = 58
+print("berat badan :",berat_badan, "kg")
+tinggi_badan = 164
+print("tinggi badan :",tinggi_badan, "cm")
+saudara_kandung = 0
+print("jumlah saudara kandung :",saudara_kandung)
+print(" ")
+#tipe data float
+tipe_data = "data float"
+print("tipe :",tipe_data)
+total_nilai = 72.10
+jumlah_sks = 21
+ip = float(total_nilai) / float(jumlah_sks)
+print("ip semester ganjil :",ip)
+
+
+
+
+
+
+
+
+
+

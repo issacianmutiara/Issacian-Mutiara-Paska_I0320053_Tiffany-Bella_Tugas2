02abd4e94e9e3c9b1bc150398198756553cedb4b
I Mutiara <issacianmutiara@gmail.com>
2021-03-10 11:51:56 +0700

    Tugas 2.1

---
+++ exercise 2.1.py
@@ -0,0 +1,18 @@
+#tipe data BooLean
+print(True)
+
+#tipe data string
+print("Ayo belajar Python")
+print('Belajar Python Sangat Mudah')
+
+#tipe data Integer
+print(18)
+
+#tipe data Float
+print(8.10)
+
+#tipe data Hexadecimal
+print(8)
+
+#tipe data Complex
+print(2j)

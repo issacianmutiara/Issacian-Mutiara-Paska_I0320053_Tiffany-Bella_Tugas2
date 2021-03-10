f5486a518a33911551cf9198d27162f0d1b0c64d
I Mutiara <issacianmutiara@gmail.com>
2021-03-10 21:12:39 +0700

    Soal 1

---
+++ Soal 1.py
@@ -0,0 +1,35 @@
+#perhitungan luas persegi panjang
+print("\nLuas persegi panjang\n")
+p = float(input("masukkan nilai panjang: "))
+l = float(input("masukkan nilai lebar: "))
+Luas = p * l
+print("\nluas persegi panjang =\n" ,Luas)
+
+#perhitungan luas lingkaran
+print("\nLuas lingkaran\n")
+r = float(input("masukkan nilai jari-jari: "))
+Luas = 3.14 * r * r
+print("\nluas lingkaran =\n" ,Luas)
+
+#perhitungan luas kubus
+print("\nLuas kubus\n")
+s = float(input("masukkan nilai panjang sisi: "))
+Luas = 6 * s * s
+print("\nluas kubus =\n" ,Luas)
+
+#Konversi suhu celcius ke fahrenhet
+print("\nMengkonversi suhu celcius ke fahrenheit\n")
+C = float(input("masukkan nilai celcius: "))
+F = (C * 9/5) + 32
+print("\nC =\n" ,F)
+
+#Konversi suhu reamur ke kelvin
+print("\nMengkonversi suhu reamur ke kelvin\n")
+R = float(input("masukkan nilai reamur: "))
+K = (R * 5/4) + 273.15
+print("\nC =\n" ,R)
+
+
+
+
+
